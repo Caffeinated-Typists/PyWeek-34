26275accab517b693cd42bfb17729eb5b96554ab
Aakarsh Jain <jainaakarsh09@gmail.com>
2022-09-05 22:36:33 +0530

    feat: Added initial code for Protagonist, needs to be debugged

---
+++ PyWeek-34/characters/protagonist.py
@@ -0,0 +1,55 @@
+import typing
+import arcade
+
+PROTAGONIST_SPEED:int = 10
+PROTAGONIST_JUMP_SPEED:int = 5
+
+class Protagonist(arcade.Sprite):
+
+    def __init__(self) -> None:
+        """Initialize the character"""
+        super().__init__(":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png")
+        self.horizontal_vel:int = PROTAGONIST_SPEED
+        self.jump_vel:int = PROTAGONIST_JUMP_SPEED
+        
+    def set_pos_x(self, x:int) -> None:
+        """Set the x coordinate of the player"""
+        self.center_x = x
+
+    def set_pos_y(self, y:int) -> None:
+        """Set the y coordinate of the player"""
+        self.center_y = y
+
+    def update_pos_x(self, x:int) -> None:
+        """Update the x coordinate of the player"""
+        self.center_x += x
+
+    def update_pos_y(self, y:int) -> None:
+        """Update the y coordinate of the player"""
+        self.center_y += y
+
+    def set_vel_x(self, x:int) -> None:
+        """Set the x velocity of the player"""
+        self.change_x = x
+
+    def set_vel_y(self, y:int) -> None:
+        """Set the y velocity of the player"""
+        self.change_y = y
+
+    def update_vel_x(self, x:int) -> None:
+        """Update the x velocity of the player"""
+        self.change_x += x
+
+    def update_vel_y(self, y:int) -> None:
+        """Update the y velocity of the player"""
+        self.change_y += y
+
+    def go_left(self) -> None:
+        """Update the speed in x direction to go left"""
+        self.set_vel_x(-self.horizontal_vel)
+
+    def go_right(self) -> None:
+        """Update the speed in x direction to go right"""
+        self.set_vel_x(self.horizontal_vel)
+
+    
