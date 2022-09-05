bb2226a22dc7c7dd0dc387a14e170b15ac3459cc
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-09-05 22:38:59 +0530

    modified color of images

---
+++ PyWeek-34/resources/Tiled Folder/Desert modified.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Desert modified" tilewidth="128" tileheight="128" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../Game Assets/deserttileset/png/Tile/13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="93" source="../Game Assets/deserttileset/png/Tile/14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="93" source="../Game Assets/deserttileset/png/Tile/15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="93" source="../Game Assets/deserttileset/png/Tile/16.png"/>
+ </tile>
+</tileset>
