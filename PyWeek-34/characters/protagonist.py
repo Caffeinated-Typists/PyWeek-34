476225d12b2f69a3f1da78f0f32cc166c5b80fb6
Aakarsh Jain <jainaakarsh09@gmail.com>
2022-09-08 02:51:04 +0530

    Added animations for more motions

--- PyWeek-34/characters/protagonist.py
+++ PyWeek-34/characters/protagonist.py
@@ -14,12 +14,18 @@ class Protagonist(arcade.Sprite):
     def __init__(self) -> None:
         """Initialize the character"""
         super().__init__(scale = PROTAGONIST_SCALING)
-        self.idle_texture:arcade.Texture = arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_flying_no_movement.png", x = 0, y = 0, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT, hit_box_algorithm='Detailed') 
-        self.jump_texture:arcade.Texture = arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_flying_no_movement.png", x = 0, y = 0, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT, hit_box_algorithm='Detailed')
-        self.fall_texture:arcade.Texture = arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_flying_no_movement.png", x = 0, y = 0, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT, hit_box_algorithm='Detailed')
-        self.flying_textures:list[arcade.Texture] = [arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_flying_no_movement.png", x = i%5 * IMAGE_PIXEL_WIDTH, y = i//5 * IMAGE_PIXEL_HEIGHT, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT, hit_box_algorithm='Detailed') for i in range(10)]
+        self.idle_texture:arcade.Texture = arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_standing_idle.png", x = 0, y = 0, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT) 
+        self.flying_textures:list[arcade.Texture] = [arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_flying_no_movement.png", x = i%5 * IMAGE_PIXEL_WIDTH, y = i//5 * IMAGE_PIXEL_HEIGHT, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT) for i in range(10)]
+        self.running_textures:list[arcade.Texture] = [arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_standing_run.png", x = i%5 * IMAGE_PIXEL_WIDTH, y = i//5 * IMAGE_PIXEL_HEIGHT, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT) for i in range(15)]
+        self.walking_textures:list[arcade.Texture] = [arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_standing_walk.png", x = i%5 * IMAGE_PIXEL_WIDTH, y = i//5 * IMAGE_PIXEL_HEIGHT, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT) for i in range(15)]
+        self.jumping_textures:list[arcade.Texture] = [arcade.load_texture("characters\jetpack character\spritesheets\__jet_pack_man_no_weapon_white_helmet_standing_jump.png", x = i%5 * IMAGE_PIXEL_WIDTH, y = i//5 * IMAGE_PIXEL_HEIGHT, width = IMAGE_PIXEL_WIDTH, height = IMAGE_PIXEL_HEIGHT) for i in range(10)]
         self.texture:arcade.Texture = self.idle_texture
+
+        self.is_flying:bool = False
+        self.is_jumping:bool = False
+        self.is_running:bool = False
         self.cur_texture:int = 0
+
         self.horizontal_vel:int = PROTAGONIST_SPEED
         self.jump_vel:int = PROTAGONIST_JUMP_SPEED
         self.set_hit_box([[33, -180], [33, -270]])
@@ -75,20 +81,31 @@ class Protagonist(arcade.Sprite):
     def jump(self) -> None:
         """Update the y-velocity for jumping"""
         self.update_vel_y(self.jump_vel)
+        self.is_jumping = True
 
     def update_animation(self, delta_time: float) -> None:
         """Update the animation of the Protagonist at every call"""
 
-        # FOR JUMPING
-        if self.change_y > 0:
-            self.texture = self.jump_texture
-        elif self.change_y < 0:
-            self.texture = self.fall_texture
-        else:
-            self.texture = self.idle_texture
+        if self.change_y == 0:
+            self.is_jumping = False
 
-        # UPDATE ANIMATIONS WHILE FLYING
-        self.cur_texture += 1
-        if self.cur_texture >= len(self.flying_textures):
-            self.cur_texture = 0
-        self.texture = self.flying_textures[self.cur_texture]
+        if self.is_flying:          #If the Protagonist has Jetpack
+            self.cur_texture += 1
+            if self.cur_texture >= len(self.flying_textures):
+                self.cur_texture = 0
+            self.texture = self.flying_textures[self.cur_texture]
+        elif self.is_jumping:     #If the Protagonist is jumping
+                self.cur_texture += 1
+                if self.cur_texture >= len(self.jumping_textures):
+                    self.cur_texture = 0
+                self.texture = self.jumping_textures[self.cur_texture]
+        elif self.is_running:   #If the Protagonist is running
+            self.cur_texture += 1
+            if self.cur_texture >= len(self.running_textures):
+                self.cur_texture = 0
+            self.texture = self.running_textures[self.cur_texture]
+        else:                   #If the Protagonist is walking
+            self.cur_texture += 1
+            if self.cur_texture >= len(self.walking_textures):
+                self.cur_texture = 0
+            self.texture = self.walking_textures[self.cur_texture]
