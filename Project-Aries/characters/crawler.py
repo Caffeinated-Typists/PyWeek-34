10c8d989bea17e471086234e5610aafa72efbea2
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-09-11 00:53:20 +0530

    reduced size of assets, reduced memory consumption

--- Project-Aries/characters/crawler.py
+++ Project-Aries/characters/crawler.py
@@ -6,7 +6,7 @@ import arcade
 sys.path.append(os.getcwd() + r"\Project-Aries")
 from characters.enemy import Enemy
 
-CRAWLER_SCALING:float = 0.08
+CRAWLER_SCALING:float = 0.15
 CRAWLER_HITPOINTS:int = 500
 CRAWLER_SPEED_RATIO = 1.33
 
@@ -15,10 +15,12 @@ class Crawler(Enemy):
     def __init__(self, position:int, game_speed:int) -> None:
         """Initialize the position"""
         super().__init__(hitpoints=CRAWLER_HITPOINTS, scale=CRAWLER_SCALING)
-        self.randomize_x(position)
-        self.center_y:int = 72   #FIX THIS
+        
+        self.randomize_x(position)   #FIX THIS
         self.change_x:float = -CRAWLER_SPEED_RATIO*game_speed
         self.load_animations()
+        self.texture = self.textures[0]
+        self.bottom:int = 64
 
     def load_animations(self) -> None:
         """Loads the png for animating the character"""
